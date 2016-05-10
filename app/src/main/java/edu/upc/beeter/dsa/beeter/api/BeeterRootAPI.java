bab50143b7809fa5f64f7782978a4f349b563946
Marc Martinez <Marc Martinez>
2016-05-10 20:38:23 +0200

    Android

---
+++ app/src/main/java/edu/upc/beeter/dsa/beeter/api/BeeterRootAPI.java
@@ -0,0 +1,19 @@
+package edu.upc.beeter.dsa.beeter.api;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BeeterRootAPI {
+
+    private Map<String, Link> links;
+
+    public BeeterRootAPI() {
+        links = new HashMap<String, Link>();
+    }
+
+    public Map<String, Link> getLinks() {
+        return links;
+    }
+
+}
---
+++ app/src/main/java/edu/upc/beeter/dsa/beeter/api/MediaType.java
@@ -0,0 +1,7 @@
+package edu.upc.beeter.dsa.beeter.api;
+
+
+public interface MediaType {
+    public final static String BEETER_API_STING = "application/vnd.beeter.api.sting+json";
+    public final static String BEETER_API_STING_COLLECTION = "application/vnd.beeter.api.sting.collection+json";
+}
